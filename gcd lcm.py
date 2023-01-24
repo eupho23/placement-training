dc6dc4f3f7438bf9a8208a0ad2dedc1715a8a2bf
eupho23 <120839793+eupho23@users.noreply.github.com>
2023-01-24 17:39:35 +0530

    Create gcd lcm.py

---
+++ gcd lcm.py
@@ -0,0 +1,7 @@
+m,n = int(input()), int(input())
+min_num = min(m,n)
+for i in range (min_num,0,-1):
+    if(m%i == 0 and n%i==0):
+        gcd=i;
+        break
+print("gcd={} lcm={}".format(gcd,m*n//gcd))
